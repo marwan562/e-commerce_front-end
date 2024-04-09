078a9a6eaeff976f203f7f17788d0d9dd17785db
marwan.hassan <mixing.gamer546@gmail.com>
2024-04-09 17:14:25 +0200

    done

--- src/components/E-Commerce/category/Category.tsx
+++ src/components/E-Commerce/category/Category.tsx
@@ -1,12 +1,11 @@
 import useGetCategories from "@hooks/useGetCategories";
 import CategoryList from "./CategoryList";
+import Loanding from "@componenets/feedback/Loading/Loanding";
 
 const Category = () => {
   const { records, status, error } = useGetCategories();
 
   let recordsCategory;
-  //Pending
-  if (status === "pending") return (recordsCategory = <p>loading</p>);
   //Success
   if (status === "success") {
     recordsCategory =
@@ -16,13 +15,13 @@ const Category = () => {
         <p>No Categories</p>
       );
   }
-  //Failed
-  if (status === "failed") return (recordsCategory = <p>{error.message}</p>);
 
   return (
-    <div className="mb-6 px-3 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 sm:px-3 gap-[40px]">
-      {recordsCategory}
-    </div>
+    <Loanding status={status} error={error}>
+      <div className="mb-6 px-3 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 sm:px-3 gap-[40px]">
+        {recordsCategory}
+      </div>
+    </Loanding>
   );
 };
 
---
+++ src/components/feedback/Loading/Loanding.tsx
@@ -0,0 +1,40 @@
+import { TError, TStatus } from "@toolkit/common/types";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  status: TStatus;
+  error: TError;
+};
+
+const Loanding = ({ children, status, error }: Props) => {
+  return (
+    <div className="   ">
+      {status === "pending" ? (
+        <progress className="progress w-[22rem] container  m-auto  pb-[40px]  h-screen flex items-center justify-center"></progress>
+      ) : status === "failed" ? (
+        <div role="alert" className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>Error! {error}.</span>
+        </div>
+      ) : (
+        children
+      )}
+    </div>
+  );
+};
+
+export default Loanding;
+
