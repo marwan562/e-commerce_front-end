87847d95905cac39a8118d878277dd167694d4f1
marwan.hassan <mixing.gamer546@gmail.com>
2024-04-16 22:18:36 +0200

    Done.

---
+++ src/components/feedback/FailedError/FailedError.tsx
@@ -0,0 +1,24 @@
+import { TError } from "@types";
+
+const FailedError = ({ error }: { error: TError }) => {
+  return (
+    <div role="alert" className="alert alert-error">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="stroke-current  shrink-0 h-7 w-7"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>Error! {error}.</span>
+    </div>
+  );
+};
+
+export default FailedError;
--- src/components/feedback/Loading/Loanding.tsx
+++ src/components/feedback/Loading/Loanding.tsx
@@ -4,6 +4,7 @@ import ProductSkeleton from "../Skeletons/ProductSkeleton/ProductSkeleton";
 import CartSkeletons from "../Skeletons/CartSkeletons/CartSkeletons";
 import CategoriesSkeletons from "../Skeletons/CategoriesSkeletons/CategoriesSkeletons";
 import CartMenuSkeletons from "../Skeletons/CartMenu/CartMenuSkeletons";
+import FailedError from "../FailedError/FailedError";
 
 const skeletonsTypes = {
   product: ProductSkeleton,
@@ -26,22 +27,7 @@ const Loanding = ({ children, status, error, Type }: Props) => {
       {status === "pending" ? (
         <Componenet />
       ) : status === "failed" ? (
-        <div role="alert" className="alert alert-error">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="stroke-current  shrink-0 h-7 w-7"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>Error! {error}.</span>
-        </div>
+        <FailedError error={error} />
       ) : (
         children
       )}
